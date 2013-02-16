02318f03ad8297601ce98ea1df020f59deb3d5d5
lumianph <lumianph@cef9f782-224b-65e7-00b6-28866cbb217c>
2013-02-16 06:51:57 +0000

    first version with correct results

--- glda/src/constants.h
+++ glda/src/constants.h
@@ -1,24 +1,11 @@
 /*
- * Copyright (C) 2007 by
+ * Copyright (C) 2013 by
  * 
- * 	Xuan-Hieu Phan
- *	hieuxuan@ecei.tohoku.ac.jp or pxhieu@gmail.com
- * 	Graduate School of Information Sciences
- * 	Tohoku University
+ * 	Mian Lu
+ *	lumianph@gmail.com
  *
- * GibbsLDA++ is a free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
- *
- * GibbsLDA++ is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GibbsLDA++; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * GLDA is a free software. It is modified based on GibbsLDA++ (http://gibbslda.sourceforge.net/)
+ * GLDA accelerates GibbsLDA++ with GPU acceleration
  */
 
 #ifndef _CONSTANTS_H
