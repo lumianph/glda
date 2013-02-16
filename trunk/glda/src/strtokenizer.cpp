047b3c94e098747517e2700e21c2308f154fbbd0
lumianph <lumianph@cef9f782-224b-65e7-00b6-28866cbb217c>
2013-02-16 06:51:57 +0000

    first version with correct results

--- trunk/glda/src/strtokenizer.cpp
+++ trunk/glda/src/strtokenizer.cpp
@@ -1,25 +1,4 @@
-/*
- * Copyright (C) 2007 by
- * 
- * 	Xuan-Hieu Phan
- *	hieuxuan@ecei.tohoku.ac.jp or pxhieu@gmail.com
- * 	Graduate School of Information Sciences
- * 	Tohoku University
- *
- * GibbsLDA++ is a free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.
- *
- * GibbsLDA++ is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GibbsLDA++; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- */
+
 
 #include <string>
 #include <vector>
